dba98b72d395a0ab4c52545cf1220c76ee28eb6b
clement <clement@keiruaprod.fr>
2017-12-17 14:14:56 +0100

    solved d16

--- d16/src/lib.rs
+++ d16/src/lib.rs
@@ -1,11 +1,12 @@
+#![allow(unused_variables,unused_mut,dead_code)]
 extern crate regex;
 use regex::Regex;
 use std::str::FromStr;
 
 #[derive(Debug)]
 pub enum Instruction {
-    Spin(i32),
-    Exchange(i32,i32),
+    Spin(usize),
+    Exchange(usize,usize),
     Partner(char,char)
 }
 
@@ -18,13 +19,13 @@ pub fn extract_program (s:&str) -> Vec<Instruction> {
     for current_instr in s.split(",") {
         if re_spin.is_match (current_instr) {
             let caps = re_spin.captures(current_instr).unwrap();
-            let x:i32 = i32::from_str(caps.get(1).map_or("", |m| m.as_str())).unwrap();
+            let x:usize = usize::from_str(caps.get(1).map_or("", |m| m.as_str())).unwrap();
             instruction.push(Instruction::Spin(x));
         }
         else if re_exchange.is_match (current_instr) {
             let caps = re_exchange.captures(current_instr).unwrap();
-            let a:i32 = i32::from_str(caps.get(1).map_or("", |m| m.as_str())).unwrap();
-            let b:i32 = i32::from_str(caps.get(2).map_or("", |m| m.as_str())).unwrap();
+            let a:usize = usize::from_str(caps.get(1).map_or("", |m| m.as_str())).unwrap();
+            let b:usize = usize::from_str(caps.get(2).map_or("", |m| m.as_str())).unwrap();
             instruction.push(Instruction::Exchange(a,b));
         }
         else if re_partner.is_match (current_instr) {
@@ -36,6 +37,50 @@ pub fn extract_program (s:&str) -> Vec<Instruction> {
     }
 
     instruction
+}
+
+fn spin(v: &mut Vec<char>, x: usize) {
+    let mut w:Vec<char> = Vec::new();
+    for i in 0..x {
+        w.push(v[v.len()-x+i]);
+    }
+
+    for i in 0..v.len()-x {
+        w.push(v[i]);
+    }
+    v.clear();
+    for i in 0..w.len() {
+        v.push(w[i]);
+    }
+}
+
+fn exchange(v: &mut Vec<char>, a: usize, b: usize) {
+    let z = v[a];
+    v[a] = v[b];
+    v[b] = z;
+}
+fn partner(v: &mut Vec<char>, a: char, b: char) {
+    let x = v.iter().position(|&x| x == a).unwrap();
+    let y = v.iter().position(|&y| y == b).unwrap();
+    exchange(v, x, y);
+}
+
+pub fn create_program(nb_programs: u8) -> Vec<char>{
+    let mut v:Vec<char> = Vec::new();
+    for i in 0..nb_programs {
+        v.push(('a' as u8 + i) as char);
+    }
+    v
+}
+
+pub fn run (v: &mut Vec<char>, instructions: &Vec<Instruction>) {
+    for instr in instructions {
+        match *instr {
+            Instruction::Spin(x) => spin(v, x),
+            Instruction::Exchange(a,b) => exchange(v, a, b),
+            Instruction::Partner(a,b) => partner(v, a, b),
+        }
+    }
 }
 
 #[cfg(test)]
