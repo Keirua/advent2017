4a1fc409f48bed199e9b3cfab8b12fd5162d99a4
clement <clement@keiruaprod.fr>
2017-12-18 23:20:38 +0100

    solving d17 part2

--- d17/src/bin/main.rs
+++ d17/src/bin/main.rs
@@ -2,16 +2,28 @@ extern crate d17;
 
 use d17::*;
 
+fn solve_p2(nb_iterations:usize, max_value:i32) {
+    let mut pos:usize = 0;
+    let mut next_value = 0;
+
+    for current_value in 1..max_value+1 {
+        pos = (pos + nb_iterations) % (current_value as usize);
+        // Every time we insert in position 0, the value after 0 changes
+        if pos == 0 {
+        	next_value = current_value;
+        }
+        pos = pos +1;
+    }
+    println!("{:?}", next_value);
+}
+
 fn main() {
     let nb_iterations = 370;
     let max_value = 2017;
     let v:Vec<i32> = generate_spinlock(nb_iterations, max_value);
 
-
-    let pos_of_2017 =  v.iter().position(|&x| x == 2017).unwrap();
+    let pos_of_2017 =  v.iter().position(|&x| x == max_value).unwrap();
     println!("{:?}", v[pos_of_2017+1]);
 
-    let v2:Vec<i32> = generate_spinlock(nb_iterations, 50000000);
-    let pos_of_0 =  v2.iter().position(|&x| x == 0).unwrap();
-    println!("{:?}", v2[pos_of_0+1]);
+    solve_p2(nb_iterations, 50000000);
 }
