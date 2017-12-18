0332ea57224a29ef7e27fba1598076190a784879
clement <clement@keiruaprod.fr>
2017-12-18 23:05:54 +0100

    added brute force part2

--- d17/src/bin/main.rs
+++ d17/src/bin/main.rs
@@ -7,5 +7,11 @@ fn main() {
     let max_value = 2017;
     let v:Vec<i32> = generate_spinlock(nb_iterations, max_value);
 
-    println!("{:?}", v);
+
+    let pos_of_2017 =  v.iter().position(|&x| x == 2017).unwrap();
+    println!("{:?}", v[pos_of_2017+1]);
+
+    let v2:Vec<i32> = generate_spinlock(nb_iterations, 50000000);
+    let pos_of_0 =  v2.iter().position(|&x| x == 0).unwrap();
+    println!("{:?}", v2[pos_of_0+1]);
 }
